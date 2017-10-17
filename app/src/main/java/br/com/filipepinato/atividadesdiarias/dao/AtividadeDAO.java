f516c35518ca19ad6517f7fa65b8f9bf31d2832c
fihpinato <fpinatoaguiar@gmail.com>
2017-10-17 15:26:37 -0200

    .

--- app/src/main/java/br/com/filipepinato/atividadesdiarias/dao/AtividadeDAO.java
+++ app/src/main/java/br/com/filipepinato/atividadesdiarias/dao/AtividadeDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 import br.com.filipepinato.atividadesdiarias.model.Atividade;
 
 public class AtividadeDAO {
-    public List<Atividade> findAll(){
+    public List<Atividade> findAll() {
         return new Select()
                 .from(Atividade.class)
                 .orderBy("descricao ASC")
